17314ada9d45f7ee29a539cfb51425e42da4680b
harshv46 <59475272+harshv46@users.noreply.github.com>
2020-09-29 16:24:52 +0530

    added splash page and changed the base theme (#3)

---
+++ app/src/main/java/com/example/lostandfound/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.lostandfound
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    private val runnable = Runnable {
+        if(!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 1500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
