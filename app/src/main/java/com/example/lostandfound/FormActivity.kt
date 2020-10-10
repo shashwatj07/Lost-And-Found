c808441a506234fa71f7b13ba710a5773809511a
Harsh Vardhan <harshvardhan21364@gmail.com>
2020-10-10 15:23:37 +0530

    FormActivity added to post lost/found request, configured the nav bar accordingly

--- app/src/main/java/com/example/lostandfound/FormActivity.kt
+++ app/src/main/java/com/example/lostandfound/FormActivity.kt
@@ -11,27 +11,66 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import android.widget.RadioButton
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_form.*
 
-class MainActivity : AppCompatActivity() {
-
+class FormActivity : AppCompatActivity() {
     lateinit var drawerLayout: DrawerLayout
     lateinit var linearLayout: LinearLayout
     lateinit var toolbar: Toolbar
     lateinit var navigationView: NavigationView
-    var previousMenuItem:MenuItem?=null
+    var previousMenuItem: MenuItem?=null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_form)
+
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        postformbutton.setOnClickListener{
+            val itemname:String = item_name.text.toString()
+            val desecription:String = description.text.toString()
+            val date:String = dateid.text.toString()
+            val id = radiogroup.checkedRadioButtonId
+            val radioButton=findViewById<RadioButton>(id)
+            val posttype:String= radioButton.text.toString()
+            val user= auth.currentUser
+            if(user == null){
+                Toast.makeText(baseContext, "Authentication Failed", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val currentuser=user.uid
+                if(posttype.isEmpty()||desecription.isEmpty()||date.isEmpty()){
+                    Toast.makeText(this@FormActivity, "Fields cannot be left blank", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    val form = formdataClass(date,desecription)
+                    database.child(posttype).child(currentuser).child(itemname).setValue(form)
+                    description.text.clear()
+                    dateid.text.clear()
+                    item_name.text.clear()
+                    Toast.makeText(this@FormActivity, "Request Posted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         drawerLayout = findViewById(R.id.drawer_layout)
         linearLayout = findViewById(R.id.linearLayout)
         toolbar = findViewById(R.id.toolbar)
         navigationView = findViewById(R.id.navigationView)
         setUpToolbar()
-        supportActionBar?.title="Main Activity"
-        navigationView.setCheckedItem(R.id.nav_item_one)
-        val actionBarDrawerToggle=ActionBarDrawerToggle(this@MainActivity,drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        supportActionBar?.title="Lost/Found Request"
+        navigationView.setCheckedItem(R.id.nav_item_four)
+        val actionBarDrawerToggle=
+            ActionBarDrawerToggle(this@FormActivity,drawerLayout, R.string.open_drawer, R.string.close_drawer)
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
@@ -45,9 +84,8 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId)
             {
                 R.id.nav_item_one->{
-                    supportActionBar?.title="My Profile"
-                    navigationView.setCheckedItem(R.id.nav_item_one)
-                    drawerLayout.closeDrawers()
+                    val intent = Intent(this@FormActivity, MainActivity::class.java)
+                    startActivity(intent)
                 }
 
                 R.id.nav_item_two->{
@@ -60,8 +98,7 @@ class MainActivity : AppCompatActivity() {
                     drawerLayout.closeDrawers()
                 }
                 R.id.nav_item_four->{
-
-                    val intent = Intent(this@MainActivity, FormActivity::class.java)
+                    val intent = Intent(this@FormActivity, FormActivity::class.java)
                     startActivity(intent)
                 }
                 R.id.nav_item_five->{
@@ -86,7 +123,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.nav_item_nine->{
                     FirebaseAuth.getInstance().signOut()
-                    val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                    val intent = Intent(this@FormActivity, LoginActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
                 }
@@ -94,7 +131,6 @@ class MainActivity : AppCompatActivity() {
             return@setNavigationItemSelectedListener true
         }
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id=item.itemId
         if(id==android.R.id.home)
@@ -114,5 +150,4 @@ class MainActivity : AppCompatActivity() {
         if(drawerLayout.isDrawerOpen(GravityCompat.START))
             drawerLayout.closeDrawers()
     }
-
 }
---
+++ app/src/main/java/com/example/lostandfound/formdataClass.kt
@@ -0,0 +1,6 @@
+package com.example.lostandfound
+
+data class formdataClass(
+    var Date:String?=" ",
+    var Description:String?=" "
+)
