9729ffb3d6647196f460ef0ce7022ca926cd8a5c
Samarth Singh Pawar <samarths@iitbhilai.ac.in>
2020-10-11 18:11:10 +0530

    Added Found Activity as well as Lost Activity

--- app/src/main/java/com/example/lostandfound/FoundActivity.kt
+++ app/src/main/java/com/example/lostandfound/FoundActivity.kt
@@ -1,83 +1,92 @@
 package com.example.lostandfound
 
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
-import android.widget.RadioButton
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_form.*
-import java.time.LocalDateTime
+import kotlinx.android.synthetic.main.activity_lost.*
 
-class FormActivity : AppCompatActivity() {
+class FoundActivity : AppCompatActivity() {
     lateinit var drawerLayout: DrawerLayout
-    lateinit var linearLayout: LinearLayout
     lateinit var toolbar: Toolbar
     lateinit var navigationView: NavigationView
+    lateinit var recyclerView: RecyclerView
+    lateinit var progressBar: ProgressBar
     var previousMenuItem: MenuItem?=null
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
-
-        auth = Firebase.auth
+        setContentView(R.layout.activity_found)
         database = Firebase.database.reference
+        database.keepSynced(true)
+        var lostItemsList: MutableList<Item> = arrayListOf()
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                val temp = dataSnapshot.child("Found").children
+                for (item in temp){
+                    var desc = item.child("description").value.toString()
+                    var itemName = item.child("itemname").value.toString()
+                    var lostFoundDate = "Date Found: "+item.child("lostfoundDate").value.toString()
+                    var postById = item.child("postedbyid").value.toString()
+                    var person = "Found By: "+dataSnapshot.child("users").child(postById).child("name").value.toString()
+                    var tstamp = "Posted at: "+item.child("tstamp").value.toString()
+                    lostItemsList.add(Item(desc,lostFoundDate,person,itemName,tstamp))
+                }
+//
+//                Log.i("Last mein","Main List of items are: $lostItemsList")
 
-        postformbutton.setOnClickListener{
-            var tstamp= LocalDateTime.now().toString()
-            val itemname:String = item_name.text.toString()
-            val desecription:String = description.text.toString()
-            val lostfoundDate:String = dateid.text.toString()
-            val id = radiogroup.checkedRadioButtonId
-            val radioButton=findViewById<RadioButton>(id)
-            val posttype:String= radioButton.text.toString()
-            val user= auth.currentUser
-            if(user == null){
-                Toast.makeText(baseContext, "Authentication Failed", Toast.LENGTH_SHORT).show()
             }
-            else{
-                val currentuser=user.uid
-                if(posttype.isEmpty()||desecription.isEmpty()||lostfoundDate.isEmpty()){
-                    Toast.makeText(this@FormActivity, "Fields cannot be left blank", Toast.LENGTH_SHORT).show()
-                }
-                else{
-                    val form = formdataClass(currentuser,itemname,lostfoundDate,desecription,tstamp)
-                    val key=database.child(posttype).push().key.toString()
-                    database.child(posttype).child(key).setValue(form)
-                    database.child("users").child(currentuser).child(posttype).push().setValue(key)
-                    description.text.clear()
-                    dateid.text.clear()
-                    item_name.text.clear()
-                    Toast.makeText(this@FormActivity, "Request Posted", Toast.LENGTH_SHORT).show()
-                }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("LostActivity", "loadPost:onCancelled", databaseError.toException())
+                // ...
             }
         }
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recycler_view.layoutManager = layoutManager
 
+        val adapter = LostAdapter(this, lostItemsList)
+        recycler_view.adapter = adapter
+        database.addValueEventListener(postListener)
+        //Once fetched then viewing the lost objects
+        progressBar = findViewById(R.id.progressBar)
+        recyclerView = findViewById(R.id.recycler_view)
+        progressBar.visibility = View.GONE
+        recyclerView.visibility = View.VISIBLE
+
+        //Navigation drawer
         drawerLayout = findViewById(R.id.drawer_layout)
-        linearLayout = findViewById(R.id.linearLayout)
         toolbar = findViewById(R.id.toolbar)
         navigationView = findViewById(R.id.navigationView)
         setUpToolbar()
-        supportActionBar?.title="Lost/Found Request"
-        navigationView.setCheckedItem(R.id.nav_item_four)
+        supportActionBar?.title="Found Items"
+        navigationView.setCheckedItem(R.id.nav_item_three)
         val actionBarDrawerToggle=
-            ActionBarDrawerToggle(this@FormActivity,drawerLayout, R.string.open_drawer, R.string.close_drawer)
+            ActionBarDrawerToggle(this@FoundActivity,drawerLayout, R.string.open_drawer, R.string.close_drawer)
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
@@ -91,18 +100,18 @@ class FormActivity : AppCompatActivity() {
             when(it.itemId)
             {
                 R.id.nav_item_one->{
-                    val intent = Intent(this@FormActivity, MainActivity::class.java)
+                    val intent = Intent(this@FoundActivity, MainActivity::class.java)
                     startActivity(intent)
                 }
 
                 R.id.nav_item_two->{
-                    startActivity(Intent(this@FormActivity, LostActivity::class.java))
+                    startActivity(Intent(this@FoundActivity, LostActivity::class.java))
                 }
                 R.id.nav_item_three->{
-                    startActivity(Intent(this@FormActivity, FoundActivity::class.java))
+                    startActivity(Intent(this@FoundActivity, FoundActivity::class.java))
                 }
                 R.id.nav_item_four->{
-                    val intent = Intent(this@FormActivity, FormActivity::class.java)
+                    val intent = Intent(this@FoundActivity, FormActivity::class.java)
                     startActivity(intent)
                 }
                 R.id.nav_item_five->{
@@ -127,7 +136,7 @@ class FormActivity : AppCompatActivity() {
                 }
                 R.id.nav_item_nine->{
                     FirebaseAuth.getInstance().signOut()
-                    val intent = Intent(this@FormActivity, LoginActivity::class.java)
+                    val intent = Intent(this@FoundActivity, LoginActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
                 }
@@ -135,6 +144,7 @@ class FormActivity : AppCompatActivity() {
             return@setNavigationItemSelectedListener true
         }
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id=item.itemId
         if(id==android.R.id.home)
@@ -154,4 +164,7 @@ class FormActivity : AppCompatActivity() {
         if(drawerLayout.isDrawerOpen(GravityCompat.START))
             drawerLayout.closeDrawers()
     }
+
 }
+
+
---
+++ app/src/main/java/com/example/lostandfound/Item.kt
@@ -0,0 +1,9 @@
+package com.example.lostandfound
+
+data class Item(
+    var description: String? = "",
+    var lostFoundDate: String? = "",
+    var lostBy: String? = "",
+    var itemName: String? = "",
+    var tstamp:String?=" "
+)
