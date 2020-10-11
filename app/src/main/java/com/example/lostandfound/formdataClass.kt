b901dbbb09f5508c9c306f89452c47fdf6e6aec5
harshv46 <59475272+harshv46@users.noreply.github.com>
2020-10-11 08:52:52 +0530

    FormActivity added to post lost/found request, configured the nav bar accordingly (#8)
    * FormActivity added to post lost/found request, configured the nav bar accordingly
    
    * changed database configuration, added unique id to each post and some more data fields
    
    * modified database to store lost/found post id in respective users field, minor change in layout of form

---
+++ app/src/main/java/com/example/lostandfound/formdataClass.kt
@@ -0,0 +1,9 @@
+package com.example.lostandfound
+
+data class formdataClass(
+    var postedbyid:String?=" ",
+    var itemname:String?=" ",
+    var lostfoundDate:String?=" ",
+    var Description:String?=" ",
+    var tstamp:String?=" "
+)
