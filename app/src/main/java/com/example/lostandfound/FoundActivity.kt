84f1754a5c70fdc298d490a75915e374eba83c6e
Samarth Singh Pawar <62646546+SamSaver@users.noreply.github.com>
2020-10-14 06:57:00 +0530

    Samarth (#10)
    * Added Found Activity as well as Lost Activity
    
    * Upgraded UI
    
    Co-authored-by: harshv46 <59475272+harshv46@users.noreply.github.com>

--- app/src/main/java/com/example/lostandfound/FoundActivity.kt
+++ app/src/main/java/com/example/lostandfound/FoundActivity.kt
@@ -40,6 +40,8 @@ class FoundActivity : AppCompatActivity() {
         setContentView(R.layout.activity_found)
         database = Firebase.database.reference
         database.keepSynced(true)
+        var colorList: MutableList<String> = arrayListOf("#81ecec","#74b9ff","#a29bfe","#00cec9","#0984e3","#6c5ce7")
+
         var lostItemsList: MutableList<Item> = arrayListOf()
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -52,7 +54,8 @@ class FoundActivity : AppCompatActivity() {
                     var postById = item.child("postedbyid").value.toString()
                     var person = "Found By: "+dataSnapshot.child("users").child(postById).child("name").value.toString()
                     var tstamp = "Posted at: "+item.child("tstamp").value.toString()
-                    lostItemsList.add(Item(desc,lostFoundDate,person,itemName,tstamp))
+                    lostItemsList.add(Item(desc,lostFoundDate,person,itemName,tstamp,colorList[(0..5).random()]))
+
                 }
 //
 //                Log.i("Last mein","Main List of items are: $lostItemsList")
--- app/src/main/java/com/example/lostandfound/LostAdapter.kt
+++ app/src/main/java/com/example/lostandfound/LostAdapter.kt
@@ -1,6 +1,10 @@
 package com.example.lostandfound
 
+
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +13,15 @@ import kotlinx.android.synthetic.main.list_item.view.*
 
 class LostAdapter(val context: Context, val lostItems: List<Item>) : RecyclerView.Adapter<LostAdapter.MyViewHolder>() {
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        @SuppressLint("Range")
         fun setData (item: Item?, pos: Int) {
             itemView.lostTitle.text = item!!.itemName
             itemView.lostDesc.text = item!!.description
             itemView.lostBy.text = item!!.lostBy
             itemView.lostFoundDate.text = item!!.lostFoundDate
             itemView.tstamp.text = item!!.tstamp
+            itemView.itemBox.setBackgroundColor(Color.parseColor(item!!.color))
         }
     }
 
@@ -31,4 +38,6 @@ class LostAdapter(val context: Context, val lostItems: List<Item>) : RecyclerVie
         val itemLost = lostItems[position]
         holder.setData(itemLost, position)
     }
+
 }
+//itemLost.color!!
